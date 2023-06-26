206451bbe567604cd5b53da521ee3791b46953bd
Huỳnh Gia Phú <20521752@gm.uit.edu.vn>
2023-06-26 20:47:23 +0700

    login with Google firebase

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,14 +1,53 @@
-import React from "react";
+import React, { useEffect } from "react";
+
+import { app } from "../config/firebase.config";
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
 
 import { FcGoogle } from "react-icons/fc";
+import { useNavigate } from "react-router-dom";
 
-const Login = () => {
+const Login = ({ setAuth }) => {
+  const firebaseAuth = getAuth(app);
+  const provider = new GoogleAuthProvider();
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (window.localStorage.getItem("auth") === "true") {
+      navigate("/", { replace: true });
+    }
+  }, []);
+
+  const loginWithGoogle = async () => {
+    await signInWithPopup(firebaseAuth, provider).then((userCred) => {
+      if (userCred) {
+        setAuth(true);
+        window.localStorage.setItem("auth", "true");
+
+        firebaseAuth.onAuthStateChanged((userCred) => {
+          if (userCred) {
+            userCred.getIdToken().then((token) => {
+              console.log(token);
+            });
+            navigate("/", { replace: true });
+          } else {
+            setAuth(false);
+            navigate("/login");
+          }
+        });
+      }
+    });
+  };
+
   return (
     <div className="relative w-screen h-screen">
       <div className="absolute inset-0 bg-darkOverlay flex items-center justify-center p-4">
         <div className="w-full md:w-375 p-4 bg-lightOverlay shadow-2xl rounded-md backdrop-blur-md flex flex-col items-center justify-center">
-          <div className="flex items-center justify-center gap-2 px-4 py-2 rounded-md bg-cardOverlay cursor-pointer hover:bg-card hover:shadow-md duration-100 ease-in-out transition-all">
-            <FcGoogle className="text-xl"/>
+          <div
+            className="flex items-center justify-center gap-2 px-4 py-2 rounded-md bg-cardOverlay cursor-pointer hover:bg-card hover:shadow-md duration-100 ease-in-out transition-all"
+            onClick={loginWithGoogle}
+          >
+            <FcGoogle className="text-xl" />
             Sign in with Google
           </div>
         </div>
--- src/config/firebase.config.js
+++ src/config/firebase.config.js
@@ -2,12 +2,12 @@ import { getApp, getApps, initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAbcuvZZUQeRhoYgt1XZDzYuo8pwTCbiiE",
-  authDomain: "melodia-music-app.firebaseapp.com",
-  projectId: "melodia-music-app",
-  storageBucket: "melodia-music-app.appspot.com",
-  messagingSenderId: "691994025219",
-  appId: "1:691994025219:web:a6aa554d2e943dbc759d36",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGIN_ID,
+  appId: process.env.REACT_APP_FIREBASE_API_ID,
 };
 
 const app = getApps.length > 0 ? getApp() : initializeApp(firebaseConfig);
