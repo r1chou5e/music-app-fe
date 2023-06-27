fe5b5629be4ac587d30fefb2f9d781b5b6635c8a
Huỳnh Gia Phú <20521752@gm.uit.edu.vn>
2023-06-27 12:27:48 +0700

    add header in Home

---
+++ src/components/Header.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Logo } from "../assets/img";
+import { NavLink } from "react-router-dom";
+import { isActiveStyle, isNotActiveStyle } from "../utils/styles";
+
+const Header = () => {
+  return (
+    <header className="flex items-center w-full p-4 md:py-2 md:px-6">
+      <NavLink to={"/"}>
+        <img src={Logo} alt="Logo" className="w-16 " />
+      </NavLink>
+      <ul className="flex items-center justify-center ml-7">
+        <li className="mx-5 text-lg">
+          <NavLink
+            to={"/home"}
+            className={({isActive}) => isActive ? isActiveStyle : isNotActiveStyle}
+          >
+            Home
+          </NavLink>
+        </li>
+        <li className="mx-5 text-lg">
+          <NavLink to={"/musics"}>Musics</NavLink>
+        </li>
+        <li className="mx-5 text-lg">
+          <NavLink to={"/premium"}>Premium</NavLink>
+        </li>
+        <li className="mx-5 text-lg">
+          <NavLink to={"/contact"}>Contact Us</NavLink>
+        </li>
+      </ul>
+    </header>
+  );
+};
+
+export default Header;
