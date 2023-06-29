51deb1829a24e6f54f62d7d060514d34484c9a35
Huỳnh Gia Phú <20521752@gm.uit.edu.vn>
2023-06-29 17:38:42 +0700

    popup to set role

--- src/components/DashboardUserCard.jsx
+++ src/components/DashboardUserCard.jsx
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { AiOutlineCheck } from "react-icons/ai";
 import moment from "moment";
+import { useStateValue } from "../context/StateProvider";
+import { motion } from "framer-motion";
 
 const DashboardUserCard = ({ data, index }) => {
-
-  
+  const [{ user }, dispatch] = useStateValue();
+  const [isUserRoleUpdated, setIsUserRoleUpdated] = useState(false);
+  const createdAt = moment(new Date(data.createdAt)).format(
+    "MMMM Do YYYY, h:mm:ss a"
+  );
 
   return (
-    <div className="relative w-full rounded-md flex items-center justify-between py-4 bg-lightOverlay cursor-pointer hover:bg-card hover:shadow-md">
+    <motion.div
+      initial={{ opacity: 0, y: 50 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.3, delay: index * 0.1 }}
+      className="relative w-full rounded-md flex items-center justify-between py-4 bg-lightOverlay cursor-pointer hover:bg-card hover:shadow-md"
+    >
       <div className="w-275 min-w-[160px] flex items-center justify-center">
         <img
           src={data.imageUrl}
@@ -31,12 +41,33 @@ const DashboardUserCard = ({ data, index }) => {
         )}
       </p>
       <p className="text-base text-textColor w-275 min-w-[160px] text-center">
-        {data.createdAt}
+        {createdAt}
       </p>
-      <p className="text-base text-textColor w-275 min-w-[160px] text-center">
-        {data.role}
-      </p>
-    </div>
+
+      <div className="w-275 min-w-[160px] text-center flex items-center justify-center gap-6 relative">
+        <p className="text-base text-textColor w-275 min-w-[160px] text-center">
+          {data.role}
+        </p>
+        {data._id !== user?.user._id && (
+          <motion.p
+            whileTap={{ scale: 0.75 }}
+            className="text-[10px] font-semibold text-textColor px-1 bg-purple-200 rounded-sm hover:shadow-md"
+            onClick={() => setIsUserRoleUpdated(true)}
+          >
+            {data.role === "admin" ? "member" : "admin"}
+          </motion.p>
+        )}
+
+        {isUserRoleUpdated && (
+          <motion.div className="absolute z-10 top-6 righ-4 p-4 flex items-start flex-col gap-4 bg-white shadow-xl rounded-md">
+            <p className="text-textColor text-sm font-semibold">
+              Are you sure to set this user as{" "}
+              <span>{data.role === "admin" ? "member" : "admin"} ?</span>
+            </p>
+          </motion.div>
+        )}
+      </div>
+    </motion.div>
   );
 };
 
